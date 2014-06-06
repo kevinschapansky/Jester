9a054cb007ace6cfbdeedc65028802d13f520bea
Kevin Schapansky <kschapansky.dev@gmail.com>
2014-06-05 21:13:25 -0700

    Added time keeping ability

--- include/SceneGraphNode.h
+++ include/SceneGraphNode.h
@@ -11,7 +11,62 @@
 #include <glm/mat4x4.hpp>
 #include <glm/gtc/matrix_transform.hpp>
 
+#include <float.h>
+#include <string>
+#include <sys/time.h>
+
 namespace jester {
+	class TimeData {
+	public:
+		double max;
+		double min;
+		double total;
+		double average;
+		unsigned int measurements;
+		double startTime;
+
+		TimeData() {
+			max = -DBL_MAX;
+			min = DBL_MAX;
+			total = 0.0;
+			average = 0.0;
+			measurements = 0;
+			startTime = 0.0;
+		}
+
+		static double getWallTime() {
+			struct timeval time;
+			if (gettimeofday(&time,NULL)){
+				//  Handle error
+				return 0;
+			}
+			return (double)time.tv_sec + (double)time.tv_usec * .000001;
+		}
+
+		void start() {
+			startTime = getWallTime();
+		}
+
+		void stopAndCalculate() {
+			double elapsed = getWallTime() - startTime;
+
+			if (elapsed > max)
+				max = elapsed;
+			if (elapsed < min)
+				min = elapsed;
+			total += elapsed;
+			measurements++;
+			average = total / measurements;
+		}
+
+		void print(std::string name) {
+			printf("%s:\n", name.c_str());
+			printf("\tMax: %f\n", max);
+			printf("\tMin: %f\n", min);
+			printf("\tAverage: %f over %d\n", average, measurements);
+		}
+	};
+
 	class SceneGraphNode {
 	public:
 		std::vector<SceneGraphNode *> children();
